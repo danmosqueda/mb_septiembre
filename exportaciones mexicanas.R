f78d525d4ed5ec9e85020593ff011bc65e7dd108
danmosqueda <dmp@sai.com.mx>
2020-09-09 19:17:13 -0500

    datos

--- exportaciones mexicanas.R
+++ exportaciones mexicanas.R
@@ -9,8 +9,12 @@ library(readxl)
 
 #### Carga de datos ####
 
-piechart_raw <- read_excel("input_piechart.xlsx") 
+piechart_raw <- read_excel("input_piechart.xlsx")
 
+input_ensamblaje_mex <- read_excel("input ensamblaje mex.xlsx") %>% 
+  rename(Marca = Makes, Modelo = Carlines) %>% 
+  select(!Manufacturer) %>% 
+  select(!Marca)
 
 piechart <- piechart_raw%>% 
   select(Año, Marca, `País destino`, Cantidad) %>% 
@@ -19,3 +23,27 @@ piechart <- piechart_raw%>%
   replace(is.na(.), 0) %>% 
   group_by(Marca) %>% 
   summarise(Cantidad = sum(Cantidad))
+
+write.csv(piechart, "datos de 2019 de exportación de vehículos a estados unidos.csv")
+
+
+ligeros <- piechart_raw%>% 
+  select(Año, Marca, `País destino`, Cantidad, Segmento) %>% 
+  filter(`País destino` == "Estados Unidos") %>% 
+  filter(Año == "2019") %>% 
+  replace(is.na(.), 0) %>% 
+  group_by(Segmento) %>% 
+  summarise(Cantidad = sum(Cantidad))
+
+write.csv(ligeros, "datos de 2019 de coches ligeros y pickups.csv")
+
+
+#### 
+
+contenido_regional <- piechart_raw%>% 
+  select(Año, Marca, Modelo, `País destino`, Cantidad, Segmento) %>% 
+  filter(`País destino` == "Estados Unidos") %>% 
+  filter(Año == "2019") %>% 
+  left_join(input_ensamblaje_mex) %>% 
+  filter(Cumplimiento_contenido == 1) %>% 
+  summarise( exp_cont = sum(Cantidad))
